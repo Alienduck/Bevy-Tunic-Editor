0545ba8c3fc900b49e592b352680c01d79a2a53d
Owen Neuillyr <jacquonneries@gmail.com>
2026-04-17 23:20:58 +0200

    init the main context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+mod camera;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(camera::CameraPlugin)
+        .add_systems(Startup, setup_world)
+        .run();
+}
+
+fn setup_world(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        DirectionalLight::default(),
+        Transform::from_xyz(3.0, 5.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Sphere::new(1.))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(1.0, 0., 0.),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
 }
